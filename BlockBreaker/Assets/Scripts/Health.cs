7cff477b2045b31defe22336d29eb75636d3538a
Ethan Cragun <craguned@gmail.com>
2017-10-17 11:34:43 -0600

    Paddle moving and walls

---
+++ BlockBreaker/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+
+
+	void OnCollisionEnter2D (Collision2D myCollider) {
+
+		//Take away health
+		health--;
+		//if health is < 0 destroy brick
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+		
+		
+	}
+	
+}
