cd080c89bb470edecc5e4b29be56df695e2bd1e8
Ethan Cragun <craguned@gmail.com>
2017-10-24 11:56:57 -0600

    Helmet Starts with Paddle/Brick Image Changes

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public GameObject paddle;
+
+	private bool playing = false;
+	private Vector3 paddleToBallVector;  //distance from ball to paddle
+	private Rigidbody2D rigid;
+
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		rigid = this.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+
+		if (!playing) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		
+		
+			//if push start button
+			if (Input.GetMouseButtonDown (0)) {
+				
+				//ball goes flying
+				rigid.velocity = new Vector2 (4,20);
+				playing = true;
+			}
+
+		
+		
+		}
+		
+		
+		}
+}
--- BlockBreaker/Assets/Scripts/Health.cs
+++ BlockBreaker/Assets/Scripts/Health.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class Health : MonoBehaviour {
 
 	public int health;
-
+	public Sprite[] picture;
+	private int count = 0;
 
 	void OnCollisionEnter2D (Collision2D myCollider) {
 
 		//Take away health
 		health--;
+		count++;
 		//if health is < 0 destroy brick
+
+		//change the picture
+
 		if (health <= 0) {
 			Destroy (this.gameObject);
 		}
 		
-		
+		GetComponent<SpriteRenderer> ().sprite = picture [count];
 	}
 	
 }
