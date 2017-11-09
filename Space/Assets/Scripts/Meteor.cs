784bee7dbeac3ab9d4a603f1b5a740bbb2b0212d
Ethan Cragun <craguned@gmail.com>
2017-11-08 22:08:27 -0700

    Space shooter started
    Laser, meteor, and player sprites/scripts

---
+++ Space/Assets/Scripts/Meteor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public int health;
+//public Sprite[] picture;
+//private int count = 0;
+
+public class Meteor : MonoBehaviour {
+
+	public float startingSpin;
+
+
+	void Start () {
+		GetComponent<Rigidbody2D> ().AddTorque (Random.Range(-startingSpin,startingSpin),ForceMode2D.Impulse);
+
+//		levelManager = FindObjectOfType<LevelManager> ();
+//	}
+
+//	void OnCollisionEnter2D (Collision2D myCollider) {
+
+		//Take away health
+//		health--;
+//		count++;
+//		if (count > picture.Length - 1) {
+//			count--;
+//		}
+		//if health is < 0 destroy brick
+//		GetComponent<SpriteRenderer>().sprite = picture[count];
+		//change the picture
+//		if (health <= 0) {
+//			LevelManager.brickCount--;
+//			levelManager.CheckBrickCount ();
+//			Destroy (this.gameObject);
+
+//	}
+	
+
+//	void Update () {	
+//		}
+
+	}
+
+}
---
+++ Space/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+//speed variable
+	public GameObject projectile;
+	public Transform shotPos;
+	public float shotForce;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject shot = Instantiate (projectile, shotPos.position, shotPos.rotation) as GameObject;
+			shot.GetComponent<Rigidbody2D> ().AddForce (shotPos.up * shotForce);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y+=0.35f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y-=0.35f;
+			this.transform.position = position;
+		}	
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x+=0.35f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x-=0.35f;
+			this.transform.position = position;
+		}
+
+
+		//check for button push
+		//print something out
+		//move our player  speed
+		//down
+		//left
+		//right
+		//up
+
+
+	}
+}
