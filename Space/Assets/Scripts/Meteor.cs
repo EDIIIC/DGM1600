03e38cba0be59d2ec71fbc5989a8bf63de66e4b9
Ethan Cragun <craguned@gmail.com>
2017-11-30 13:07:18 -0700

    Meteor Movement?
    Tried to figure out how to make meteors chase ship

--- Space/Assets/Scripts/Meteor.cs
+++ Space/Assets/Scripts/Meteor.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour {
 
-	public int health;
-	public Sprite[] picture;
-	private int count = 0;
+	//public int health;
+	//public Sprite[] picture;
+	//private int count = 0;
 	public float startingSpin;
 	public float speed;
 
-	private GameObject Player;
+	private GameObject player;
 	private Rigidbody2D rigid;
 
 	void Start () {
 		
-		Player = FindObjectOfType<PlayerController> ().gameObject;
+		player = FindObjectOfType<PlayerController> ().gameObject;
 		rigid = GetComponent<Rigidbody2D> ();
 
 		//Where to go      
-		transform.right = Player.transform.position - transform.position;
+		transform.right = player.transform.position - transform.position;
 
 		//speed
 		rigid.AddRelativeForce(new Vector2(Random.Range(0,speed),0f),ForceMode2D.Force);
 
 		//Random Rotation
-		rigid.AddTorque (Random.Range(-startingSpin,startingSpin),ForceMode2D.Impulse);
+		//rigid.AddTorque (Random.Range(-startingSpin,startingSpin),ForceMode2D.Impulse);
 
 	}
 		
+	/*
 	private void OnCollisionEnter2D(Collision2D coll){
 		coll.gameObject.GetComponent<Health> ().IncrementHealth (-1);
 
@@ -46,6 +47,7 @@ public class Meteor : MonoBehaviour {
 		}
 
 	}
+	*/
 
 }
 
